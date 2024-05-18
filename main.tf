8c328295f95803ead0cd4add617a4bd710b02c51
Vlada Rudyk <vlada.rudyk.2015@gmail.com>
2024-05-18 16:38:33 +0300

    initial test

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "google_storage_bucket" "auto-expire" {
+  name          = "pigeon-cicd-bucket"
+  location      = "US"
+  project = "pigeons-419911"
+  force_destroy = true
+  public_access_prevention = "enforced"
+}
