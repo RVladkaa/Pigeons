c1a805f2a77ea46b42795ab130170b4a52caf271
Vlada Rudyk <vlada.rudyk.2015@gmail.com>
2024-05-18 18:22:29 +0300

    pushin to prod branch

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
+provider "google" {
+   credentials = "${file("./creds/serviceaccount.json")}"
+   project     = "pigeons-419911" 
+   region      = "US"
+ }
+
 resource "google_storage_bucket" "auto-expire" {
   name          = "pigeon-cicd-bucket"
   location      = "US"
   project = "pigeons-419911"
   force_destroy = true
   public_access_prevention = "enforced"
+  
 }
-#
+
 resource "google_storage_bucket" "auto-expire2" {
   name          = "pigeon-cicd-bucket-2"
   location      = "US"
